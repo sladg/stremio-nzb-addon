5101e7fd02f62244bd0d6afaf33e32de43a9ff57
Sleeyax <yourd3veloper@gmail.com>
2025-11-28 16:28:05 +0100

    refactor: reorganize code structure

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { router } from "./nzbhydra/router.js";
+import { router as nzbHydraRouter } from "./nzbhydra/router.js";
 
 const app = express();
 const port = process.env.PORT ? +process.env.PORT : 3000;
 
-app.use("/nzbhydra", router);
+app.use("/nzbhydra", nzbHydraRouter);
 
 app.listen(port, () => {
   console.log(`Addon listening at http://localhost:${port}`);
