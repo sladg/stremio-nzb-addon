fba51baae06f438ff426fcb8c4abeeca90bbbd6b
Sleeyax <yourd3veloper@gmail.com>
2025-11-28 15:44:37 +0100

    chore: remove useless code

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT ? +process.env.PORT : 3000;
 
 app.use("/nzbhydra", router);
-app.get("/nzbhydra2", (req, res) => res.redirect("/nzbhydra"));
 
 app.listen(port, () => {
   console.log(`Addon listening at http://localhost:${port}`);
